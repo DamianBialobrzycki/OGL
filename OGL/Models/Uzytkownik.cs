5e97857269b5727d59eb6f16608083b50f7e7c40
Damian Białobrzycki <damianbialobrzycki@gmail.com>
2017-11-25 20:39:01 +0100

    Utworzenie modelu danych aplikacji.

---
+++ OGL/Models/Uzytkownik.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace OGL.Models
+{
+    public class Uzytkownik : IdentityUser
+    {
+        public Uzytkownik()
+        {
+            this.Ogloszenia = new HashSet<Ogloszenie>();
+        }
+
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+
+        #region dodatkowe pole notmapped
+
+        [NotMapped]
+        [Display(Name = "Pan/Pani:")]
+        public string PelneNazwisko
+        {
+            get { return Imie + " " + Nazwisko; }
+        }
+
+        #endregion
+
+        public virtual ICollection<Ogloszenie> Ogloszenia { get; private set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<Uzytkownik> manager)
+        {
+            // Element authenticationType musi pasować do elementu zdefiniowanego w elemencie CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Dodaj tutaj niestandardowe oświadczenia użytkownika
+            return userIdentity;
+        }
+    }
+}
